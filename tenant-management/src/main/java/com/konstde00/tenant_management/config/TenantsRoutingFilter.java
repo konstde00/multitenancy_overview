cf7110251bb3d611f70be4e02e376e4dcb0e4be6
kostiantyndementiev <kostiantyn.dementiev@observepoint.com>
2022-12-09 16:29:12 +0200

    updated tenant routing filter

--- tenant-management/src/main/java/com/konstde00/tenant_management/config/TenantsRoutingFilter.java
+++ tenant-management/src/main/java/com/konstde00/tenant_management/config/TenantsRoutingFilter.java
@@ -1,15 +1,14 @@
-package com.konstde00.auth.config;
+package com.konstde00.tenant_management.config;
 
 import com.konstde00.tenant_management.service.data_source.DataSourceContextHolder;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 @Order(2)
@@ -18,7 +17,8 @@ public class TenantsRoutingFilter extends OncePerRequestFilter {
     DataSourceContextHolder dataSourceContextHolder;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    @SneakyThrows
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
 
         dataSourceContextHolder.updateTenantContext(request);
 
